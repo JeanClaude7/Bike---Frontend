87978330fbe0f41fb7513a2d1445a8abd5e73990
sharon <sharon0manua@gmail.com>
2024-04-17 13:10:17 +0200

    lll

---
+++ script.js
@@ -0,0 +1,58 @@
+fetch("http://localhost:3000/bike")
+.then((response) => response.json())
+.then((data) => console.log(data));
+
+//Mise en place de l'envent click 1 pour ZOOK07
+const addBtn1 = document.querySelector('#addBtn1');
+//(verification) console.log(addBtn);
+addBtn1.addEventListener('click',function(){
+//(verification)  console.log("test")
+const ZOOK07 = document.querySelector('#ZOOK07')
+console.log(ZOOK07);
+})
+
+//Mise en place de l'envent click 1 pour ZOOK07
+const addBtn2 = document.querySelector('#addBtn2');
+//(verification) console.log(addBtn);
+addBtn2.addEventListener('click',function(){
+//(verification)  console.log("test")
+const TITANS = document.querySelector('#TITANS')
+console.log(TITANS);
+})
+
+//Mise en place de l'envent click 1 pour ZOOK07
+const addBtn3 = document.querySelector('#addBtn3');
+//(verification) console.log(addBtn);
+addBtn3.addEventListener('click',function(){
+//(verification)  console.log("test")
+const CEWO = document.querySelector('#CEWO')
+console.log(CEWO);
+})
+
+//Mise en place de l'envent click 1 pour ZOOK07
+const addBtn4 = document.querySelector('#addBtn4');
+//(verification) console.log(addBtn);
+addBtn4.addEventListener('click',function(){
+//(verification)  console.log("test")
+const AMIG039 = document.querySelector('#AMIG039')
+console.log(AMIG039);
+})
+
+//Mise en place de l'envent click 1 pour ZOOK07
+const addBtn5 = document.querySelector('#addBtn5');
+//(verification) console.log(addBtn);
+addBtn5.addEventListener('click',function(){
+//(verification)  console.log("test")
+const BIK046 = document.querySelector('#BIK046')
+console.log(BIK046);
+})
+
+//Mise en place de l'envent click 1 pour ZOOK07
+const addBtn6 = document.querySelector('#addBtn6');
+//(verification) console.log(addBtn);
+addBtn6.addEventListener('click',function(){
+//(verification)  console.log("test")
+const LIK099 = document.querySelector('#LIK099')
+console.log(LIK099);
+})
+
